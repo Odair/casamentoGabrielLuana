08e21a23e753fe95b8d60e79039c54bfa197b7c7
Andre Macedo Tertuliano <andre.tertuliano@prodaptech.com.br>
2016-05-23 09:15:19 -0300

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+.config(function($routeProvider){
+
+
+$routeProvider.when('/fotos', {
+                templateUrl : 'partials/principal.html',
+                controller:'FotosController'
+});
+
+$routeProvider.when('/fotos/new', {
+                templateUrl : 'partials/fotos.html',
+});
+
+$routeProvider.otherwise({redirectTo : '/fotos'});
+
+});
---
+++ public/site/js/controllers/fotos-controller.js
@@ -0,0 +1,15 @@
+angular.module("alurapic").controller('FotosController', function($scope, $http){
+
+$scope.fotos = [];
+$scope.filtro = '';
+
+
+        $http.get('v1/fotos')
+        .success(function(data){
+            $scope.fotos = data;
+        }).error(function(error){
+            console.log(error);
+        })
+
+
+});
